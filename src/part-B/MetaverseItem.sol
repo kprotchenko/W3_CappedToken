0c388cdd7641b9d542c138ccff5f637f548c7cd0
Kyrylo Protchenko <32849961+kprotchenko@users.noreply.github.com>
2025-11-10 13:19:21 -0500

    Additional contract changes for Part-B 5

--- src/part-B/MetaverseItem.sol
+++ src/part-B/MetaverseItem.sol
@@ -13,7 +13,7 @@ contract MetaverseItem is ERC721, ERC721Royalty, ERC721Enumerable, AccessControl
     uint16 public constant MAX_SUPPLY = 10_000;
     uint256 private _nextTokenId;
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-    string public immutable baseURI;
+    string public baseURI;
 
     // B-2: Constructor (name, symbol, baseURI, admin) sets default 5 % royalty and grants MINTER_ROLE to admin.
     constructor(string memory name_, string memory symbol_, string memory _baseURI, address _admin)
@@ -40,6 +40,7 @@ contract MetaverseItem is ERC721, ERC721Royalty, ERC721Enumerable, AccessControl
     }
 
     // B-4: setBaseURI(string) – only admin; stores IPFS base (e.g., ipfs://CID/).
+    // Todo: Changing base URI is not a good idea as it would break tokenURI(uint256 tokenId) function
     function setBaseURI(string memory _baseURI) external onlyRole(DEFAULT_ADMIN_ROLE) {
         baseURI = _baseURI;
     }
@@ -50,6 +51,7 @@ contract MetaverseItem is ERC721, ERC721Royalty, ERC721Enumerable, AccessControl
     }
 
     // B-5.2: tokenURI = baseURI + tokenId + “.json”.
+    // Todo: Changing base URI is not a good idea as doing so would break this method
     function tokenURI(uint256 tokenId) public view override(ERC721) returns (string memory) {
         _requireOwned(tokenId);
         string memory baseURI = _baseURI();
--- test/part-B/MetaverseItem.t.sol
+++ test/part-B/MetaverseItem.t.sol
@@ -7,10 +7,16 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MetaverseItemTest is Test {
     MetaverseItem public metaverseItem;
+    string public baseURI;
+
+    address public itemAdmin;
 
     function setUp() public {
+        baseURI = vm.envString("BASE_URI");
+        itemAdmin = payable(address(vm.envAddress("ITEM_ADMIN")));
+        metaverseItem = new MetaverseItem("MetaverseItem1", "MI1", itemAdmin);
     }
-    //mint increments id & respects captokenURI
+    //mint increments id & respects cap tokenURI
     //returns expected IPFS link
-
+    // ipfs://CID/
 }
